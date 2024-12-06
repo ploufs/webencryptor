bd5f7082e59e5947730cce788fc3e0e7a1ef9482
Francis Noel <ploufs@users.noreply.github.com>
2024-12-06 13:28:27 -0500

    Add project files.

---
+++ publish.ps1
@@ -0,0 +1,15 @@
+# source: https://www.meziantou.net/create-a-multi-arch-docker-image-for-a-dotnet-application.htm
+# Create a new web project
+#dotnet new web
+
+# Create 2 images for x64 and ARM64
+$registry = "ghcr.io"
+$image = "ploufs/webencryptor"
+$tag = "latest"
+
+dotnet publish -p:PublishProfile=DefaultContainer --configuration Release --os linux --arch x64 -p:ContainerImageTag=$tag-x64 -p:ContainerRepository=$image -p:ContainerRegistry=$registry
+dotnet publish -p:PublishProfile=DefaultContainer --configuration Release --os linux --arch arm64 -p:ContainerImageTag=$tag-arm64 -p:ContainerRepository=$image -p:ContainerRegistry=$registry
+
+#combine the images into a manifest and push the manifest to the registry
+docker manifest create "$registry/${image}:$tag" "$registry/${image}:$tag-x64" "$registry/${image}:$tag-arm64"
+docker manifest push "$registry/${image}:$tag"
