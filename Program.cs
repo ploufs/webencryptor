6f2d66776618f42af8f91bfa01f4fcb7372ddd5b
Francis Noel <ploufs@users.noreply.github.com>
2024-12-06 13:46:30 -0500

    ajout openApi

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// ajout openApi
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, _, _) =>
+    {
+        document.Info.Title = "webencryptor";
+        document.Info.Description = "";
+        document.Info.Version = "v1";
+        return Task.CompletedTask;
+    });
+});
+
 var app = builder.Build();
+
+// ajout openApi endpoint (/openapi/v1.json)
+app.MapOpenApi();
 
 app.MapGet("/", () => "Hello World!");
 
