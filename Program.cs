55ab7a20e49235adccda03377d4f1bb61aa4bd5e
Francis Noel <ploufs@users.noreply.github.com>
2024-12-14 16:58:57 -0500

    config openapi / scalar enabled in appsetting

--- Program.cs
+++ Program.cs
@@ -12,34 +12,51 @@ var configuration = new ConfigurationBuilder()
 // aller chercher les configuration de php dans appsettings
 var pgpConfig = configuration.GetRequiredSection("Pgp").Get<PgpConfig>();
 
+var openApiEnabled = configuration.GetRequiredSection("OpenApiEnabled").Get<bool>();
+var scalarEnabled = configuration.GetRequiredSection("ScalarEnabled").Get<bool>();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // ajout openApi
-builder.Services.AddOpenApi(options =>
-{
-    options.AddDocumentTransformer((document, _, _) =>
+if (openApiEnabled)
+    builder.Services.AddOpenApi(options =>
     {
-        document.Info.Title = "webencryptor";
-        document.Info.Description = "";
-        document.Info.Version = "v1";
-        return Task.CompletedTask;
+        options.AddDocumentTransformer((document, _, _) =>
+        {
+            document.Info.Title = "webencryptor";
+            document.Info.Description = "";
+            document.Info.Version = "v1";
+            return Task.CompletedTask;
+        });
     });
-});
 
 // ajouter les configurations de Scalar
-builder.Services.AddOptions<ScalarOptions>().BindConfiguration("Scalar");
+if (scalarEnabled)
+    builder.Services.AddOptions<ScalarOptions>().BindConfiguration("Scalar");
 
 var app = builder.Build();
 
 // ajout openApi endpoint (/openapi/v1.json)
-app.MapOpenApi();
+if (openApiEnabled)
+    app.MapOpenApi();
 
 // ajouter scalar (/scalar/v1)
 // source : https://github.com/scalar/scalar/blob/main/documentation/integrations/dotnet.md , https://github.com/scalar/scalar/blob/main/packages/scalar.aspnetcore/README.md)
-app.MapScalarApiReference();
+if (scalarEnabled)
+    app.MapScalarApiReference();
 
-// redirect to scalar documentation
-app.MapGet("/", () => Results.Redirect("/scalar/v1"));
+// redirect to documentation
+if (openApiEnabled || scalarEnabled)
+    app.MapGet("/", () =>
+    {
+        if (scalarEnabled)
+            return Results.Redirect("/scalar/v1");
+
+        if (openApiEnabled)
+            return Results.Redirect("/openapi/v1.json");
+
+        return Results.Empty;
+    });
 
 app.MapPost("/PGPEncrypt", async ([FromForm] string filenamePublicKey, [FromForm] string text) =>
 {
--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 `docker pull ploufs/webencryptor:latest`
 ### environement variable
 ASPNETCORE_HTTP_PORTS -> 8080
+OpenApiEnabled -> true,false
+ScalarEnabled -> true,false
 ### volume
 /app/PGPPublicKey -> pgp public key folder
 ### run image
@@ -17,5 +19,8 @@ ASPNETCORE_HTTP_PORTS -> 8080
 openapi : http://localhost:8080/openapi/v1.json
 scalar: http://localhost:8080/scalar/v1
 
-## exemple
+## exemple powershell
 curl https://localhost:8080/PGPEncrypt --request POST --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'filenamePublicKey={{publickey}}' --data-urlencode 'text={{text}}'
+
+## exemple shell script (linux)
+message_chiffre=$(curl -s -F "filenamePublicKey={{publickey}}" -F "text={{text}}" https://localhost:8080/PGPEncrypt);
